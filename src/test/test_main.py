fc884225d395f25d7cd60bf8063642c92b33794c
Timofey <nikiforov.ta@edu.spbstu.ru>
2022-04-16 15:57:04 +0300

    minor fix

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,11 +1,11 @@
 import json
 import os.path
 
-from src import main
+from src.main import handler
 
 
 def test_handler(repo_root):
     with open(os.path.join(repo_root, 'request.json'), 'rb') as request_examples:
         for request in json.load(request_examples):
-            print(json.loads(main.handler(request, None)))
+            print(json.loads(handler(request, None)))
     assert True
